e361dc3acefe733fc700aa3145b2bddfa6654249
Denisson Leal <denissonleal@gmail.com>
2024-07-27 17:56:01 +0100

    instalacao e configuracao do laravel socialite

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,4 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    // App\Providers\PassportSocialiteServiceProvider::class,
-    // \SocialiteProviders\Manager\ServiceProvider::class,
 ];
