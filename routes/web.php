b0aeafab0dbd4ac4bc878b2dcf74e9bb4cc47b91
Denisson Leal <denissonleal@gmail.com>
2024-08-02 19:49:33 +0100

    coloque nome da rota como login

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/redirect', function () {
+Route::get('/login', function () {
     return Socialite::driver('laravelpassport')->redirect();
 });
 
